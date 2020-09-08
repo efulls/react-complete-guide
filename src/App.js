8c4c2f9eebaadfcefb52173a1de656f75d5a3188
efulls <saeful.mahmud@gmail.com>
2020-09-08 22:53:41 +0700

    Working With Css Modules

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 
-import './App.css';
+import classes from './App.css';
 import Person from './Person/Person';
 
 class App extends Component {
@@ -49,20 +49,10 @@ class App extends Component {
 
 
   render() {
-    const style = {
-      backgroundColor:'green',
-      color:'white',
-      font:'inherit',
-      border:'1px solid blue',
-      padding:'8px',
-      cursor:'pointer',
-      ':hover':{
-        backgroundColor:'lightgreen',
-        color:'black'
-      }
-    };
-
     let persons = null;
+    let btnClass = '';
+
+
     if(this.state.showPersons){
       persons =(
         <div>
@@ -76,27 +66,24 @@ class App extends Component {
           })}
         </div>
       );
-      // style.backgroundColor='red';
-      // style[':hover']={
-      //   backgroundColor:'salmon',
-      //   color:'black'
-      // };
+      
+      btnClass = classes.Red;
     }
 
     // let classes =['red','bold'].join(' ');
-    const classes =[];
+    const assignedClasses =[];
     if(this.state.persons.length <=2){
-      classes.push('red');//clasess =['red]
+      assignedClasses.push(classes.red);//clasess =['red]
     }
     if(this.state.persons.length <=1){
-      classes.push('bold');//clasess = ['red bold']
+      assignedClasses.push(classes.bold);//clasess = ['red bold']
     }
 
     return (
-        <div className="App">
+        <div className={classes.App}>
           <h1>Hi, iam react App</h1>
-          <p className={classes.join(' ')}>This is really working!</p>
-          <button className="button" onClick={this.togglePersonsHandler}>
+          <p className={assignedClasses.join(' ')}>This is really working!</p>
+          <button className={btnClass} onClick={this.togglePersonsHandler}>
             Toggle Persons
           </button>
           {persons}
