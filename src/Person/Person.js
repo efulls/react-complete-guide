cd5d743e127eb85a128e2fb2b11f0bbe28068ffb
efulls <saeful.mahmud@gmail.com>
2020-09-08 09:54:06 +0700

    Adding and using Radium, npm install --save radium

--- src/Person/Person.js
+++ src/Person/Person.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import './Person.css';
+import Radium from 'radium';
 
 
 const person = (props) => {
@@ -14,4 +15,4 @@ return (
     )
 };
 
-export default person;
+export default Radium(person);
